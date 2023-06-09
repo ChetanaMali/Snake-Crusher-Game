ea26c40dc3be56a66e84ef388a6799e461c24d94
Chetana Mali <65480069+ChetanaMali@users.noreply.github.com>
2023-06-10 00:36:18 +0530

    Update CameraFollow.cs

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -9,11 +9,7 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private float smoothTime;
     private Vector3 _currentVelocity = Vector3.zero;
 
-    Vector2 turn;
-    float sensitivity = 0.5f;
-    // Start is called before the first frame update
     
-
     // Update is called once per frame
     private void LateUpdate()
     {
